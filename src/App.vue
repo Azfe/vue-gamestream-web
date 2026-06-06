6370a84024b93b78caaf5234a7097829068e7d67
Alex Zapata <alexzapata1984@gmail.com>
2026-06-06 12:44:36 +0200

    Add Consumo de APIs y Objetos Reactivos en Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
-<script setup></script>
+<script setup>
+// import LayoutHero from "@/components/Layout/LayoutHero.vue";
+import { onMounted, reactive } from 'vue'
+
+const API_URL = 'https://gamestreamapi.herokuapp.com/api/games'
+
+const state = reactive({
+  error: null,
+  isLoading: false,
+  data: []
+})
+
+const fetchGames = async () => {
+  try {
+    state.isLoading = true
+    const response = await fetch(API_URL)
+    const json = await response.json()
+    state.data = json
+    return state.data
+  } catch (error) {
+    state.error = error
+  } finally {
+    state.isLoading = false
+  }
+}
+
+onMounted(async () => {
+  const gamesData = await fetchGames()
+  state.data.push(...gamesData)
+})
+
+</script>
 
 <template>
-  <h1>Hola mundo</h1>
+  <!-- <LayoutHero /> -->
+  <main></main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  padding: 2rem;
+}
+
+</style>
