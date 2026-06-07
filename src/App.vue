ed5145a44cfe0ee51f66e3bd0b656cda3164e405
Alex Zapata <alexzapata1984@gmail.com>
2026-06-07 10:04:37 +0200

    feat: wire GameLayout named slots and search filter state in App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { onMounted, reactive, ref } from 'vue'
 import LayoutHero from "@/components/Layout/LayoutHero.vue";
-import GameCard from "@/components/Games/GameCard.vue";
-import { onMounted, reactive } from 'vue'
+import GameLayout from "@/components/Games/GameLayout.vue";
+import GameCard from "@/components/Games/GameCard.vue"
+import GameModal from "@/components/Games/GameModal.vue";
+import SharedLoader from "@/components/Shared/SharedLoader.vue";
 
 const API_URL = 'https://gamestreamapi.herokuapp.com/api/games'
+
+const gamesView = ref([])
 
 const state = reactive({
   error: null,
@@ -17,6 +22,7 @@ const fetchGames = async () => {
     const response = await fetch(API_URL)
     const json = await response.json()
     state.data = json
+    gamesView.value = json
     return state.data
   } catch (error) {
     state.error = error
@@ -29,13 +35,22 @@ onMounted(async () => {
   await fetchGames()
 })
 
+const setGameView = (filteredGames) => {
+  gamesView.value = filteredGames
+}
+
 </script>
 
 <template>
   <LayoutHero />
   <main>
-    <!-- <GameLayout :games="state.data" /> -->
-    <GameCard v-for="game in state.data" :key="game.title" :game="game" />
+    <SharedLoader v-if="state.isLoading" />
+    <GameLayout v-else :games="state.data" @set-game-view="setGameView">
+      <GameCard v-for="game in gamesView" :key="game.title" :game="game" />
+    </GameLayout>
+    <Teleport to="body">
+      <GameModal />
+    </Teleport>
   </main>
 </template>
 
@@ -43,5 +58,4 @@ onMounted(async () => {
 main {
   padding: 2rem;
 }
-
 </style>
